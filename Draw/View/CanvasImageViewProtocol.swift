6de33ed3bf3a36a6f5777203b49164d292d68a89
horita-yuya <horitayuya@gmail.com>
2020-06-10 10:57:58 +0900

    Remove unused cancel selected method

--- Draw/View/CanvasImageViewProtocol.swift
+++ Draw/View/CanvasImageViewProtocol.swift
@@ -3,5 +3,4 @@ import UIKit
 public protocol CanvasImageViewProtocol: UIView {
     var imageView: UIImageView! { get }
     var confirmSelected: (() -> Void)? { get set }
-    var cancelSelected: (() -> Void)? { get set }
 }
